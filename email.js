fbfa02f5b8cce15360e4e7176738cedd0e8ce5fb
suibema <ekatroma@gmail.com>
2025-07-04 13:13:18 +0300

    Update email.js

--- email.js
+++ email.js
@@ -1,10 +1,29 @@
+function getTelegramUserId() {
+  if (window.Telegram && Telegram.WebApp && Telegram.WebApp.initDataUnsafe) {
+    const user = Telegram.WebApp.initDataUnsafe.user;
+    if (user && user.id) {
+      return user.id;
+    }
+  }
+  return null;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  Telegram.WebApp.ready();
+  const id = getTelegramUserId();
+  const startParam = Telegram.WebApp.initDataUnsafe?.start_param;
+  console.log("tg-id:", id);
+  window.tgUserId = id;
+  window.tgUserStartParam = startParam;
+});
+
 document.getElementById('email-form').addEventListener('submit', async function (e) {
   e.preventDefault();
   const email = document.getElementById('email').value.trim();
   const errorEl = document.getElementById('email-error');
 
   try {
-    const find_email = await fetch(`https://ndb.fut.ru/api/v2/tables/maiff22q0tefj6t/records/count?where=(E-mail,eq,${encodeURIComponent(email)})`, {
+    const find_email = await fetch(`https://ndb.fut.ru/api/v2/tables/maiff22q0tefj6t/records/count?where=(tg-id,eq,${window.tgUserId})`, {
       method: 'GET',
       headers: {
         'accept': 'application/json',
@@ -19,7 +38,7 @@ document.getElementById('email-form').addEventListener('submit', async function 
       return;
     }
 
-    const res = await fetch(`https://ndb.fut.ru/api/v2/tables/maiff22q0tefj6t/records/count?where=(E-mail,eq,${encodeURIComponent(email)})~and(Результат КОТ,neq,-1)`, {
+    const res = await fetch(`https://ndb.fut.ru/api/v2/tables/maiff22q0tefj6t/records/count?where=(tg-id,eq,${window.tgUserId})~and(Результат КОТ,neq,-1)`, {
       method: 'GET',
       headers: {
         'accept': 'application/json',
@@ -34,7 +53,7 @@ document.getElementById('email-form').addEventListener('submit', async function 
       return;
     }
 
-    localStorage.setItem('test_email', email);
+    localStorage.setItem('test_email', window.tgUserId);
     window.location.href = 'test.html';
   } catch (err) {
     console.error(err);
