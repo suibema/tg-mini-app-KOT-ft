266c921ba311283a96436985b2e1553062307ae0
suibema <ekatroma@gmail.com>
2025-09-11 21:47:15 +0300

    Update test.js

--- test.js
+++ test.js
@@ -157,7 +157,8 @@ async function submitForm(auto = false) {
         body: JSON.stringify({
           "Id": recordId,
           "Результат КОТ": score,
-          "Дата получения ответа на тест": new Date().toISOString()
+          "Дата получения ответа на тест": new Date().toISOString(),
+          "Время получения ответа на тест": new Date().toISOString().match(/\d\d:\d\d/)
         })
       });
       if (!res.ok) {
@@ -229,6 +230,7 @@ document.addEventListener('visibilitychange', () => {
 // Initialize
 restoreForm();
 startTimer();
+
 
 
 
